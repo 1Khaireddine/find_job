32214c8d51e7f973e24662ac139539498684817f
Khaireddine <khaireddine.hamdi@com-and-dev.com>
2022-09-13 16:14:29 +0100

    FEATURE-add-devise-gem
    Details
    ------------------
    * Add devise gem
    * Custom all form
    * Add letter_opener_web gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,12 @@
-//= require jquery3
+//= require jquery
+//= require jquery_ujs
 //= require popper
 //= require bootstrap-sprockets
+//= require_tree
+
+// $(document).ready(function () {
+//   setTimeout(() => {
+//     $(".notice").remove()
+//     $(".alert").remove()
+//   }, 3000);
+// })
